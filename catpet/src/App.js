ec5c9997fe03892535a13d2a5038f7af73d8f3cf
Alisson Ross <alisson@alisson.me>
2022-11-10 19:22:26 -0800

    testing cube from threejs

---
+++ catpet/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Cube from './cube.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        CatPet v 0.0
+      </header>
+      <main>
+          <Cube/>
+      </main>
+      <footer>
+          This is a test!
+      </footer>
+    </div>
+  );
+}
+
+export default App;
