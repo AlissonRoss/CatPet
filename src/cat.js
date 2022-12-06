4f70a0823627097f1d6ec977b1c4a55340fcedfe
Alisson Ross <alisson@alisson.me>
2022-12-05 18:07:09 -0800

    Rotational Controls added

--- src/cat.js
+++ src/cat.js
@@ -5,11 +5,11 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import Cat from '../src/assets/blackcat.glb';
 // import Texture from '../src/assets/texture.png';
 import Stats from 'three/examples/jsm/libs/stats.module'
-
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 
 let container;
-let camera, scene, renderer, objCat;
+let camera, scene, renderer, objCat, controls;
 scene = new THREE.Scene();
 camera = new THREE.PerspectiveCamera( 45, window.innerWidth/ window.innerHeight, 0.1, 1000 );
 //CAMERA
@@ -17,12 +17,12 @@ camera.position.set(-1, 1, 3);
 camera.lookAt( scene.position );
 
 // scene
-scene.background = new THREE.Color(0x900C3F);
+scene.background = new THREE.Color(0x000000);
 //LIGHTING
 const light = new THREE.PointLight()
-light.position.set(2, 8, 50)
+light.position.set(-1, 1, 3)
 scene.add(light)
-const ambientLight = new THREE.AmbientLight( 0xFFFFFF, 0.07);
+const ambientLight = new THREE.AmbientLight( 0xfffff0, 0.2);
 scene.add( ambientLight );
 
 scene.add( camera );
@@ -63,11 +63,15 @@ objloader.load(Cat, ( gltf ) => {
 //FOG
  scene.fog = new THREE.Fog( 0x23272a, 0.5, 1700, 4000 );
 
+ 
+
+
 //RENDERER
 renderer = new THREE.WebGLRenderer();
 container = renderer.domElement;
 renderer.setPixelRatio( window.devicePixelRatio );
 renderer.setSize( window.innerWidth, window.innerHeight - 100);
+renderer.setClearColor( 0xffffff, 1);
 document.body.appendChild( container );
 function render() {
     
@@ -78,6 +82,26 @@ const stats = Stats();
 document.body.appendChild(stats.dom);
 // var angle = 90;
 // var radius = 100; 
+
+//CONTROLS
+ controls = new OrbitControls( camera, renderer.domElement );
+ controls.listenToKeyEvents( window ); // optional
+
+ //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
+
+ controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+ controls.dampingFactor = 0.05;
+
+ controls.screenSpacePanning = false;
+
+ controls.minDistance = 1;
+ controls.maxDistance = 5;
+
+ controls.maxPolarAngle = Math.PI / 2;
+
+
+
+
 
 function animate() {
 
